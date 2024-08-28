be664a7baba36a584cdbe98cf916cb4d2b3d5153
Nəzrin Quliyeva <140939317+NazrenG@users.noreply.github.com>
2024-08-28 14:29:53 +0400

    Add project files.

---
+++ ECommerce.WebUI/Controllers/CartController.cs
@@ -0,0 +1,52 @@
+﻿using ECommerce.Business.Abstraction;
+using ECommerce.Entity.Concretes;
+using ECommerce.WebUI.Models;
+using ECommerce.WebUI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+        private readonly ICartSessionService _sessionService;
+        private readonly IProductService _productService;
+
+        public CartController(ICartService cartService, ICartSessionService sessionService, IProductService productService)
+        {
+            _cartService = cartService;
+            _sessionService = sessionService;
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> AddToCart(int productId,int page,int category)
+        {
+            var item=await _productService.GetByIdAsync(productId);
+            var cart = _sessionService.GetCart();
+            _cartService.AddList(item, cart);
+            _sessionService.SetCart(cart);
+
+            TempData.Add("message", String.Format("Your Product, {0} was added successfully to cart", item.ProductName));
+
+
+            return RedirectToAction("Index", "Product",new {page=page,category=category});
+        }
+
+        public IActionResult List() {
+            var list = new CartListViewModel
+            {
+                Cart = _sessionService.GetCart(),
+            };
+            return View(list);
+        } 
+        public IActionResult Remove(int productId)
+        {
+            var cart = _sessionService.GetCart();
+            _cartService.DeleteList(productId,cart);
+            _sessionService.SetCart(cart);
+			//TempData.Add("message", "Your Product removed successfully from cart");
+
+			return RedirectToAction("List");
+        }
+    }
+}
---
+++ ECommerce.WebUI/Models/CartListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ECommerce.Entity.Concretes;
+
+namespace ECommerce.WebUI.Models
+{
+	public class CartListViewModel
+	{
+		public Cart? Cart { get; set; }
+	}
+}
